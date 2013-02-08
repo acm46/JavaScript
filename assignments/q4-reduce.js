d2b53713e1393f0b2903e3c589bb76de5ec8763f
acm46 <anirudh.c.mohan@gmail.com>
2013-02-08 17:56:53 -0500

    condensed problem 4 solution

--- assignments/q4-reduce.js
+++ assignments/q4-reduce.js
@@ -1,16 +1,8 @@
 function reduce (xs, fn, seed) {
-  if (xs.length === 0 && typeof seed === 'undefined') 
-  	return undefined;
-  if (typeof seed === 'undefined'){
-  	seed = xs[0];
-  	var i = 1;
-  } else {
-  	var i = 0;
-  }
+  var i = seed ? 0 : 1;
   var answer = seed;
   for (i; i < xs.length; i++){
-  	answer = fn(answer, xs[i]);
+  	answer = fn(answer ? answer : xs[0], xs[i]);
   }
-  return answer;
-  
+  return answer; 
 }
