11ba268d81c064e61260bc02fad0ac726074b082
acm46 <anirudh.c.mohan@gmail.com>
2013-02-08 17:47:52 -0500

    problem 4 solved

--- assignments/q4-reduce.js
+++ assignments/q4-reduce.js
@@ -1,4 +1,16 @@
 function reduce (xs, fn, seed) {
-  // NOTE: that seed is optional; you should give the appropriate default in the function body
-  // ...
+  if (xs.length === 0 && typeof seed === 'undefined') 
+  	return undefined;
+  if (typeof seed === 'undefined'){
+  	seed = xs[0];
+  	var i = 1;
+  } else {
+  	var i = 0;
+  }
+  var answer = seed;
+  for (i; i < xs.length; i++){
+  	answer = fn(answer, xs[i]);
+  }
+  return answer;
+  
 }
