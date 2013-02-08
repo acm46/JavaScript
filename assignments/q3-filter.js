6afccf1d110c5bf6582fdeda1c7f45d868ad1ab7
acm46 <anirudh.c.mohan@gmail.com>
2013-02-08 17:34:00 -0500

    problem 3 solved

--- assignments/q3-filter.js
+++ assignments/q3-filter.js
@@ -1,3 +1,10 @@
 function filter (xs, condition) {
-  // ...
+  var filtered = [];
+
+  for (var i = 0; i < xs.length; i++){
+  	if (condition(xs[i])) filtered.push(xs[i]);
+  }
+
+  return filtered;
+
 }
